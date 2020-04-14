c595605b73304d35037f63d262d84b0ba291652b
liujianhuan <liujianhuan@qianxin.com>
2020-04-14 13:30:01 +0800

    add script

---
+++ scripts/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64996a33a8398031b9c5db062efce549";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
