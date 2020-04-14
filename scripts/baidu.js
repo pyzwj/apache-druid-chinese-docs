f3022122e4006aabd115ea87a4f9085a436bf321
liujianhuan <liujianhuan@qianxin.com>
2020-04-14 13:48:42 +0800

    add script

--- scripts/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?64996a33a8398031b9c5db062efce549";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
